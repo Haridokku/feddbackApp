4e0fca185dcb1f4ea7c1279c1a6e14333663bcda
Haridokku <124425442+Haridokku@users.noreply.github.com>
2023-03-16 13:54:28 +0530

    final

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -5,9 +5,36 @@ import Emoji from '../Emoji'
 import './index.css'
 
 class Feedback extends Component {
+  state = {isClick: false}
+
   thanking = () => {
+    this.setState({isClick: true})
+  }
+
+  renderAuthButton = () => {
+    const {isClick} = this.state
     const {resources} = this.props
-    const {loveEmojiUrl} = resources
+    const {emojis, loveEmojiUrl} = resources
+    if (isClick === false) {
+      return (
+        <div className="app-container">
+          <div className="card-container">
+            <h1 className="heading">
+              How satisfied are you with our customer support performance?
+            </h1>
+            <ul className="emojis-container">
+              {emojis.map(each => (
+                <Emoji
+                  thanking={this.thanking}
+                  key={each.id}
+                  emojiDetails={each}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      )
+    }
     return (
       <div className="card-container">
         <img src={loveEmojiUrl} alt="love emoji" className="love-emoji" />
@@ -20,26 +47,7 @@ class Feedback extends Component {
   }
 
   render() {
-    const {resources} = this.props
-    const {emojis} = resources
-    return (
-      <div className="app-container">
-        <div className="card-container">
-          <h1 className="heading">
-            How satisfied are you with our customer support performance?
-          </h1>
-          <ul className="emojis-container">
-            {emojis.map(each => (
-              <Emoji
-                thanking={this.thanking}
-                key={each.id}
-                emojiDetails={each}
-              />
-            ))}
-          </ul>
-        </div>
-      </div>
-    )
+    return <div className="container">{this.renderAuthButton()}</div>
   }
 }
 export default Feedback
